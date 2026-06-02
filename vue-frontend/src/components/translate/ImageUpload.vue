1c18ee1e8ebb4cc83ee5c2fd699e0c05b23bf2d8
MashiroSaber03 <3325273536@qq.com>
2026-06-02 13:09:15 +0800

    删除网页导入

--- vue-frontend/src/components/translate/ImageUpload.vue
+++ vue-frontend/src/components/translate/ImageUpload.vue
@@ -630,10 +630,7 @@ defineExpose({
           <span class="select-link folder-link" @click="triggerFolderSelect">
             📁 选择文件夹
           </span>
-          <span class="separator"> | </span>
-          <span class="select-link web-import-link" @click="triggerWebImport">
-            🌐 从网页导入
-          </span>
+          <span v-if="false" class="select-link web-import-link" @click="triggerWebImport"></span>
         </p>
       </div>
       
